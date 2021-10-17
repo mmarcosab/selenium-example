221c02a9471e76d444fd59e44eb9c2ef8ddae24f
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-10-16 23:36:09 -0300

    testes de login

--- src/test/java/com/example/selenium/app/AppApplicationTests.java
+++ src/test/java/com/example/selenium/app/AppApplicationTests.java
@@ -1,16 +1,28 @@
 package com.example.selenium.app;
 
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 class AppApplicationTests {
 
+	private WebDriver webDriver;
+
+	@BeforeAll
+	public static void setWebDriver(){
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+	}
+
+	@BeforeEach
+	public void init(){
+		webDriver = new ChromeDriver();
+	}
+
 	@Test
 	public void test(){
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-		WebDriver webDriver = new ChromeDriver();
 		webDriver.navigate().to("http://localhost:8080/leiloes");
 		webDriver.quit();
 	}
---
+++ src/test/java/com/example/selenium/app/LoginTests.java
@@ -0,0 +1,55 @@
+package com.example.selenium.app;
+
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class LoginTests {
+
+	private WebDriver browser;
+
+	@BeforeAll
+	public static void setWebDriver(){
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+	}
+
+	@BeforeEach
+	public void init(){
+		browser = new ChromeDriver();
+	}
+
+	@Test
+	public void testLoginValido(){
+		browser.navigate().to("http://localhost:8080/login");
+		//geralmente se usa o id dos elementos da tela para encontrar algo na tela
+		browser.findElement(By.id("username")).sendKeys("fulano");
+		browser.findElement(By.id("password")).sendKeys("pass");
+		browser.findElement(By.id("btn-submit")).click();
+		assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		assertEquals("fulano", browser.findElement(By.id("nome-usuario")).getText());
+	}
+
+	@Test
+	public void testLoginInvalido(){
+		browser.navigate().to("http://localhost:8080/login");
+		//geralmente se usa o id dos elementos da tela para encontrar algo na tela
+		browser.findElement(By.id("username")).sendKeys("fulano2");
+		browser.findElement(By.id("password")).sendKeys("pass2");
+		browser.findElement(By.id("btn-submit")).click();
+		assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		assertTrue(browser.getPageSource().contains("Usuário e senha inválidos"));
+		assertThrows(NoSuchElementException.class, ()-> browser.findElement(By.id("nome-usuario")).getText());
+
+	}
+
+	@AfterEach
+	public void quit(){
+		browser.quit();
+	}
+
+}
